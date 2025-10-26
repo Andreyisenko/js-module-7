48bc679eeb56989edc746ae4f09b9c44212a6505
Andrey Isenko <aisenko87@gmail.com>
2025-10-26 18:23:57 +0200

    add repo-js-7

---
+++ js-module-7.js
@@ -0,0 +1,21 @@
+console.log("22");
+const wrap = document.querySelector(".wraper");
+console.log(wrap);
+const list = document.createElement("ul");
+list.classList.add("list");
+wrap.append(list);
+
+const item = document.createElement("li");
+item.classList.add("item");
+item.textContent = "name";
+
+const item1 = document.createElement("li");
+item1.classList.add("item");
+item1.textContent = "firstname";
+
+const item2 = document.createElement("li");
+item2.classList.add("item");
+item2.textContent = "age";
+list.append(item, item1, item2);
+console.log(list);
+item.classList.toggle("item1");
