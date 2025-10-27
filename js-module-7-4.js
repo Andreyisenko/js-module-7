5483a73432c0ea975b27b54201eebbcfb2a75cd5
Andrey Isenko <aisenko87@gmail.com>
2025-10-27 21:31:21 +0200

    add-task-74

---
+++ js-module-7-4.js
@@ -0,0 +1,37 @@
+const courses = [
+  { title: "JavaScript", rating: 4.5, students: 120 },
+  { title: "React", rating: 4.8, students: 85 },
+  { title: "Node.js", rating: 4.2, students: 60 },
+  { title: "Python", rating: 4.9, students: 140 },
+  { title: "HTML & CSS", rating: 4.0, students: 200 },
+];
+const cours1 = document.querySelector("#courses");
+// console.log(cours1);
+const title = document.createElement("h2");
+title.textContent = "Рейтинг курсів";
+title.classList.add("title");
+// console.log(title);
+cours1.prepend(title);
+const allCurses = courses
+  .map((elem) => {
+    return `<div class="course">
+  <h3>${elem.title}</h3>
+  <p>Рeйтинг: ${elem.rating} ⭐</p>
+  <p>Кількість  студентів: ${elem.students}</p>
+  ${
+    elem.rating >= 4.5
+      ? `<p class="status"> Популярний курс: ${elem.rating} ✅</p>`
+      : `<p class="vorse"> Може бути краще: ${elem.rating} 😅</p>`
+  }
+    
+        </div>`;
+  })
+  .join("");
+// console.log(allCurses);
+cours1.insertAdjacentHTML("beforeend", allCurses);
+const wrap = document.querySelector(".course");
+// console.log(wrap);
+
+const allCurse = document.createElement("p");
+allCurse.textContent = `Всього курсів: ${courses.length}`;
+cours1.append(allCurse);
