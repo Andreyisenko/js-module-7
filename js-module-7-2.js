33331f5017f64ef50223c4a4cf81fcbdd9a2dd5f
Andrey Isenko <aisenko87@gmail.com>
2025-10-27 20:18:37 +0200

    task-7-3

--- js-module-7-2.js
+++ js-module-7-2.js
@@ -1,28 +1,30 @@
-const courses = [
-  { title: "JavaScript", price: 1500, level: "beginner" },
-  { title: "React", price: 2500, level: "intermediate" },
-  { title: "Node.js", price: 3000, level: "advanced" },
-];
-const cours = document.querySelector("#courses");
-cours.classList.add("courses");
-const title = document.createElement("h2");
-title.classList.add("title");
-title.textContent = "Список курсів";
-// console.log(title);
-const allcurses = document.createElement("p");
-allcurses.textContent = `Всього курсів ${courses.length}`;
-console.log(allcurses);
+// const courses = [
+//   { title: "JavaScript", price: 1500, level: "beginner" },
+//   { title: "React", price: 2500, level: "intermediate" },
+//   { title: "Node.js", price: 3000, level: "advanced" },
+// ];
+// const cours = document.querySelector("#courses");
+// cours.classList.add("courses");
+// const title = document.createElement("h2");
+// title.classList.add("title");
+// title.textContent = "Список курсів";
+// // console.log(title);
+// const allcurses = document.createElement("p");
+// allcurses.textContent = `Всього курсів ${courses.length}`;
+// // console.log(allcurses);
 
-const divv = courses.map((elem) => {
-  const arr = `<div class="course-card">
- <h3>${elem.title}</h3> <p>Рівень: ${elem.level}</p> <p>Ціна: ${elem.price} грн</p>
-    <hr>
-    </div>
-    `;
-  return arr;
-});
-// console.log(divv.join(""));
-// console.log(...divv);
-cours.innerHTML = divv.join("");
-cours.prepend(title);
-cours.append(allcurses);
+// const divv = courses.map((elem) => {
+//   const arr = `<div class="course-card">
+//  <h3>${elem.title}</h3> <p>Рівень: ${elem.level}</p> <p>Ціна: ${elem.price} грн</p>
+//     <hr>
+//     </div>
+//     `;
+//   return arr;
+// });
+// // console.log(typeof divv.join(""));
+// // console.log(...divv);
+// cours.innerHTML = divv.join("");
+// cours.prepend(title);
+// cours.append(allcurses);
+// const sort = courses.sort((a, b) => b.price - a.price);
+// console.log(sort);
